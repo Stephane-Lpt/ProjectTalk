e71095eae50081ced455e83fd0877e13f1fe0726
Stephane Loppinet <stephaneloppinet@gmail.com>
2023-11-22 13:12:26 +0100

    Initialization

---
+++ src/main/java/api/projecttalk/ProjectTalkApplication.java
@@ -0,0 +1,13 @@
+package api.projecttalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/projecttalk/ProjectTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package api.projecttalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
