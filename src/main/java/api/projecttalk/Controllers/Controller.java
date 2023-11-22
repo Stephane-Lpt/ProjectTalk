d2f90bd46ae7980a0c4e9bb137d6be75b1d7383a
Stephane Loppinet <stephaneloppinet@gmail.com>
2023-11-22 15:22:28 +0100

    Addition of the API entities

--- src/main/java/api/projecttalk/Controllers/Controller.java
+++ src/main/java/api/projecttalk/Controllers/Controller.java
@@ -1,4 +1,4 @@
-package api.projecttalk;
+package api.projecttalk.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
