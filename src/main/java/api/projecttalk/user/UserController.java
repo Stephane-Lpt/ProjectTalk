0b4376312eba6e9df3536da74cf00d8350f10ce6
Stephane Loppinet <stephaneloppinet@gmail.com>
2023-11-23 20:11:22 +0100

    Changing return type of Get methods to ResponseEntity<> to put an HTTPStatus with the response

--- src/main/java/api/projecttalk/user/UserController.java
+++ src/main/java/api/projecttalk/user/UserController.java
@@ -1,6 +1,7 @@
 package api.projecttalk.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import java.util.Optional;
  * class that represent the user's controller
  */
 @RestController
-@RequestMapping("/")
+@RequestMapping("/users")
 public class UserController {
 
     /**
@@ -23,27 +24,18 @@ public class UserController {
     private UserService userService;
 
     /**
-     * Write a welcome message to check if the server is running
-     * @return a welcome message
-     */
-    @GetMapping("/")
-    public String Welcome(){
-        return "Welcome to ProjectTalk";
-    }
-
-    /**
      * Display a user searched with his username
      * @param username username of the user given in the url
      * @return the user related to the username given
      */
-    @GetMapping("/users/{username}")
-    public User getUser(@PathVariable String username) {
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
         Optional<User> user = userService.getUserByUsername(username);
         // if the user is not found, throw an exception with a 404 error
         if(!user.isPresent()){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The username : " + username + " was not found");
         }
         // else return the user
-        return user.get();
+        return new ResponseEntity<User>(user.get(), HttpStatus.OK);
     }
 }
